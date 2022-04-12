4028637582298f8ab698b71e8868670bd0616ae2
Abdul Aziz (A575072) <seeazee@gmail.com>
2022-04-12 10:28:40 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,12 +2,13 @@
 ## Specifies the Region your Terraform Provider will server
 provider "aws" {
   region = "eu-west-2"
+ 
 }
 ## Specifies the S3 Bucket and DynamoDB table used for the durable backend and state locking
 
 terraform {
     backend "s3" {
-      encrypt = false
+      encrypt = true
       bucket = "azee781-s3-bucket-name"
       dynamodb_table = "azee781_dynamo_table_name"
       key = "dev/terraform.tfstate"
